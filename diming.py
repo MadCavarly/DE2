e9100a1051d4ec8daf84a15674718b75599e1f9d
Tezgel Onur Baran (256029) <256029@vutbr.cz>
2023-10-19 17:55:40 +0200

    my changes

--- diming.py
+++ diming.py
@@ -1,30 +1,28 @@
 from machine import Pin, Timer
 from machine import PWM
+import time
 
-def interruption_handler(timer):
-    pin_led.value(not pin_led.value())
-    
-def interruption_handler1(timer):
-    pin_led2.value(not pin_led2.value())
-    
-def interruption_handler2(timer):
-    pin_led3.value(not pin_led3.value())
-    
-class machine.PWM(dest, *, freq, duty_u16, duty_ns)
+led3=25
+led2=1
+led1=3
+pwm_led1=PWM(Pin(led1,mode=Pin.OUT))
+pwm_led2=PWM(Pin(led2,mode=Pin.OUT))
+pwm_led3=PWM(Pin(led3,mode=Pin.OUT))
 
-PWM(pin_led3,freq=10,)
-    
-    
-#create an object for on-board LED
-pin_led = Pin(3, mode=Pin.OUT)
-pin_led2 = Pin(1, mode=Pin.OUT)
-pin_led3 = Pin(25, mode=Pin.OUT)
+pwm_led1.freq(100)
+pwm_led2.freq(500)
+pwm_led3.freq(1000)
 
-timer0 = Timer(0) # Between 0-3 for ESP32
-timer1=  Timer(1)
-timer2=  Timer(2)
 
-timer0.init(mode=Timer.PERIODIC, period=250, callback=interruption_handler)
-timer1.init(mode=Timer.PERIODIC, period=500, callback=interruption_handler1)
-timer2.init(mode=Timer.PERIODIC, period=750, callback=interruption_handler2)
-#timer0.init(mode=Timer.PERIODIC, freq=2, callback=interruption_handler)
+while True:
+    for duty in range(0,1024,5):
+        pwm_led1.duty(duty)
+        pwm_led2.duty(duty)
+        pwm_led3.duty(duty)
+        time.sleep_ms(5)
+    for duty in range(1023,-1,-5):
+        pwm_led1.duty(duty)
+        pwm_led2.duty(duty)
+        pwm_led3.duty(duty)
+        time.sleep_ms(5)
+   
