548983dc74dd8ed7178b2fe50b0a11273d6b24c0
Tezgel Onur Baran (256029) <256029@vutbr.cz>
2023-10-19 17:15:58 +0200

    my changes

---
+++ timers.py
@@ -0,0 +1,25 @@
+from machine import Pin, Timer
+
+def interruption_handler(timer):
+    pin_led.value(not pin_led.value())
+    
+def interruption_handler1(timer):
+    pin_led2.value(not pin_led2.value())
+    
+def interruption_handler2(timer):
+    pin_led3.value(not pin_led3.value())
+    
+    
+#create an object for on-board LED
+pin_led = Pin(3, mode=Pin.OUT)
+pin_led2 = Pin(1, mode=Pin.OUT)
+pin_led3 = Pin(25, mode=Pin.OUT)
+
+timer0 = Timer(0) # Between 0-3 for ESP32
+timer1=  Timer(1)
+timer2=  Timer(2)
+
+timer0.init(mode=Timer.PERIODIC, period=250, callback=interruption_handler)
+timer1.init(mode=Timer.PERIODIC, period=500, callback=interruption_handler1)
+timer2.init(mode=Timer.PERIODIC, period=750, callback=interruption_handler2)
+#timer0.init(mode=Timer.PERIODIC, freq=2, callback=interruption_handler)
